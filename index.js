2f63b2402efa87a68289ec17bea88d5764c45751
William Crews <crewswh@gmail.com>
2021-08-19 08:51:01 -0400

    working hard

--- index.js
+++ index.js
@@ -2,19 +2,59 @@
 
 const fs = require('fs');
 const inquirer = require("inquirer");
+const markinus = require('./utils/generateMarkdown')
 
-inquirer.prompt([
+// inquirer.prompt([
+//     {
+//         name: 'greeting',
+//         message: 'What would you like to say?',
+//         type: 'input'
+//        }])
+//   .then(function(answer){
+//     console.log(answer);
+//   });
+
+// TODO: Create an array of questions for user input
+const questions = [
     {
-        name: 'greeting',
-        message: 'What would you like to say?',
-        type: 'input'
-       }])
-  .then(function(answer){
-    console.log(answer);
-  });
+        type: 'input',
+        name: 'GitHub Account',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email Address, please',
+    },
+    {
+        type: 'input',
+        name: 'repository',
+        message: 'Repository Name',
+    },
+    {
+        type: 'input',
+        name: 'headline',
+        message: 'What You Want to Say Most',
+    },
+    {
+        type: 'input',
+        name: 'real',
+        message: 'Use',
+    },
+    {
+        type: 'input',
+        name: 'continuation',
+        message: 'Continuations',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Contributors',
+    }, 
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+];
+
+inquirer.prompt(questions);
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
